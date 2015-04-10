2a2dbe2943b15d0d07d8272566ce9e4a1d5aa0aa
haoyy <haoyy@foxmail.com>
2015-04-10 16:17:37 +0800

    update
    Signed-off-by: haoyy <haoyy@foxmail.com>

--- routes/pet.js
+++ routes/pet.js
@@ -4,12 +4,12 @@ var router = express.Router();
 router.post('/list', function(req, res, next) {
 	var  mongodb = require('mongodb');
 	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-	var  db = new mongodb.Db('pet', server, {safe:true});
+	var  db = new mongodb.Db('petHome', server, {safe:true});
 	page = req.body.page;
 	page = parseInt(page);
 	db.open(function(err, db){
 		if(!err){
-			db.collection('a',{safe:true}, function(err, collection){
+			db.collection('pet',{safe:true}, function(err, collection){
 				if(err){
 					console.log(err);
 				}
@@ -30,12 +30,12 @@ router.post('/list', function(req, res, next) {
 router.post('/info', function(req, res, next) {
 	var  mongodb = require('mongodb');
 	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-	var  db = new mongodb.Db('pet', server, {safe:true});
+	var  db = new mongodb.Db('petHome', server, {safe:true});
 	page = req.body.page;
 	page = parseInt(page);
 	db.open(function(err, db){
 		if(!err){
-			db.collection('a',{safe:true}, function(err, collection){
+			db.collection('pet',{safe:true}, function(err, collection){
 				if(err){
 					console.log(err);
 				}
@@ -51,17 +51,16 @@ router.post('/info', function(req, res, next) {
 router.post('/health', function(req, res, next) {
 	var  mongodb = require('mongodb');
 	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-	var  db = new mongodb.Db('pet', server, {safe:true});
+	var  db = new mongodb.Db('petHome', server, {safe:true});
 	page = req.body.page;
 	page = parseInt(page);
 	db.open(function(err, db){
 		if(!err){
-			db.collection('a',{safe:true}, function(err, collection){
+			db.collection('pet',{safe:true}, function(err, collection){
 				if(err){
 					console.log(err);
 				}
-				collection.insert(req.body,{safe:true},function(err, result){
-					console.log(result);
+				collection.update({"id":req.body.id}, {$set:req.body}, {safe:true}, function(err, result){
 				});
 			});
 		}
@@ -73,17 +72,17 @@ router.post('/health', function(req, res, next) {
 router.post('/add', function(req, res, next) {
 	var  mongodb = require('mongodb');
 	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-	var  db = new mongodb.Db('pet', server, {safe:true});
+	var  db = new mongodb.Db('petHome', server, {safe:true});
 	page = req.body.page;
 	page = parseInt(page);
 	db.open(function(err, db){
 		if(!err){
-			db.collection('a',{safe:true}, function(err, collection){
+			db.collection('pet',{safe:true}, function(err, collection){
 				if(err){
 					console.log(err);
 				}
 				collection.insert(req.body,{safe:true},function(err, result){
-					console.log(result);
+					//console.log(result);
 					//collection.update({title:'hello'}, {$set:{number:3}}, {safe:true}, function(err, result){
 					//});
  				}); 
