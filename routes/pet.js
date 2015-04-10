8c2f1016b95bfbadb791885dd9fd035f268e816c
haoyy <haoyy@foxmail.com>
2015-04-10 11:59:36 +0800

    add apn
    Signed-off-by: haoyy <haoyy@foxmail.com>

--- routes/pet.js
+++ routes/pet.js
@@ -1,49 +1,77 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/info', function(req, res, next) {
-	json = '';
-        res.send('post petinfo');
+router.post('/list', function(req, res, next) {
+	var  mongodb = require('mongodb');
+	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+	var  db = new mongodb.Db('pet', server, {safe:true});
+	page = req.body.page;
+	page = parseInt(page);
+	db.open(function(err, db){
+		if(!err){
+			db.collection('a',{safe:true}, function(err, collection){
+				if(err){
+					console.log(err);
+				}
+				collection.count({}, function(err, count){
+					collection.find().skip((page-1)*1).limit(1).toArray(function(err,docs){
+						json = {};
+						json.total_results = count;
+						json.page = page;
+						json.results = docs;
+						res.send(json);
+					}); 
+				});
+			});
+		}
+	}); 
 });
 
-router.post('/list', function(req, res, next) {
-	json = '{ \
-    "total_results": "number",\
-    "page": "number",\
-    "results": [ \
-        { \
-            "latitude": "string",\
-            "longitude": "string",\
-            "date": "date",\
-            "id": "number",\
-            "address": "string",\
-            "nickName": "string",\
-            "thumbUrl": "string",\
-            "title?": "string",\
-            "year": "number",\
-            "sex": "number",\
-            "sterilization": "bool",\
-            "category?": "number",\
-            "hearRateArray?": [\
-                "number",\
-                "number"\
-            ],\
-            "temperatureArray?": [\
-                "number",\
-                "number"\
-            ],\
-            "lastVaccinateTime?": "date",\
-            "lastVaccine?": "string",\
-            "vaccinate?": "string",\
-            "vaccinateTime?": "date",\
-            "hasAlert": "bool",\
-            "mode": "string"\
-        }\
-    ]\
-}'
-        page = req.body.page;
-        page = parseInt(page);
-	json = JSON.stringify(json);
+router.post('/info', function(req, res, next) {
+	var  mongodb = require('mongodb');
+	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+	var  db = new mongodb.Db('pet', server, {safe:true});
+	page = req.body.page;
+	page = parseInt(page);
+	db.open(function(err, db){
+		if(!err){
+			db.collection('a',{safe:true}, function(err, collection){
+				if(err){
+					console.log(err);
+				}
+				console.log(req.body[0]);
+				console.log(req.body[1]);
+				collection.update(req.body[0], {$set:req.body[1]}, {safe:true}, function(err, result){
+					console.log(result);
+				});
+			});
+		}
+	}); 
+	json = {"result" : 0,"reason" : ""};
+	res.send(json);
+});
+
+router.post('/health', function(req, res, next) {
+	var  mongodb = require('mongodb');
+	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+	var  db = new mongodb.Db('pet', server, {safe:true});
+	page = req.body.page;
+	page = parseInt(page);
+	db.open(function(err, db){
+		if(!err){
+			db.collection('a',{safe:true}, function(err, collection){
+				if(err){
+					console.log(err);
+				}
+				console.log(req.body[0]);
+				console.log(req.body[1]);
+				collection.update(req.body[0], {$set:req.body[1]}, {safe:true}, function(err, result){
+					console.log(result);
+				});
+			});
+		}
+	}); 
+	json = {"result" : 0,"reason" : ""};
 	res.send(json);
 });
 
